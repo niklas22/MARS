ba0267f49b1ad14009e954d006b8eca6ddcdd516
Niklas Mayr <niklas93mayr@gmail.com>
2015-12-03 11:09:01 +0100

    Initial Commit

---
+++ MarsApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MarsApp
+//
+//  Created by Niklas Mayr on 03.12.15.
+//  Copyright © 2015 Niklas Mayr. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
