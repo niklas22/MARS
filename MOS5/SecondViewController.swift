7fcd70482f83512f5dbd9699ec78058d96957b3d
Niklas Mayr <niklas93mayr@gmail.com>
2015-12-04 13:58:40 +0100

    Initial Commit

---
+++ MOS5/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MOS5
+//
+//  Created by Niklas Mayr on 04.12.15.
+//  Copyright © 2015 Niklas Mayr. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
