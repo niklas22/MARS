f8770416f12bba88700a315745058e806adcb4cd
Niklas Mayr <niklas93mayr@gmail.com>
2015-12-04 15:17:17 +0100

    Add Project

---
+++ MOS5/PedoViewController.swift
@@ -0,0 +1,52 @@
+//
+//  PedoViewController.swift
+//  MarsApp
+//
+//  Created by Niklas Mayr on 03.12.15.
+//  Copyright © 2015 Niklas Mayr. All rights reserved.
+//
+
+import UIKit
+
+class PedoViewController: UIViewController {
+    @IBOutlet weak var labelStepCount: UILabel!
+    
+    var loop = true
+    var pedo:Pedometer!
+    
+    @IBAction func btnStartPressed(sender: UIButton) {
+        labelStepCount.text = "0"
+        
+        pedo.calculateSteps()
+        
+        labelStepCount.text = String(pedo.steps)
+    }
+    
+    @IBAction func btnStopPressed(sender: UIButton) {
+    //    let steps = Int(labelStepCount.text!)!
+        
+        pedo.stopCalculating()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+        // add observer
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "updateSteps", name: "StepCounter", object: nil)
+        pedo = Pedometer()
+    }
+    
+    private func updateSteps(obj: AnyObject?){
+      //  labelStepCount.text = String(pedo.steps)
+        print("mars")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
---
+++ MOS5/Pedometer.swift
@@ -0,0 +1,61 @@
+//
+//  Pedometer.swift
+//  MarsApp
+//
+//  Created by Christian Floh on 04.12.15.
+//  Copyright © 2015 Niklas Mayr. All rights reserved.
+//
+
+import Foundation
+import CoreMotion
+
+class Pedometer {
+    
+    let manager = CMMotionManager()
+    var serviceAvailable : Bool
+    var steps : Int
+    var notificationCenter = NSNotificationCenter.defaultCenter()
+    
+    init() {
+        serviceAvailable = manager.accelerometerAvailable
+        steps = 0
+        
+        if serviceAvailable {
+            manager.accelerometerUpdateInterval = 0.2
+            manager.startAccelerometerUpdates()
+           
+            
+        }
+    }
+    
+    func calculateSteps() {
+        notificationCenter.postNotificationName("StepCounter", object: nil)
+        
+        if serviceAvailable {
+            let violence = 1.6
+            
+            manager.startAccelerometerUpdatesToQueue(NSOperationQueue.mainQueue(), withHandler: { (data, error) -> Void in
+                
+                var x = data?.acceleration.x
+                var y = data?.acceleration.y
+                var z = data?.acceleration.z
+                
+                x = sqrt(pow(x!, 2))
+                y = sqrt(pow(y!, 2))
+                z = sqrt(pow(z!, 2))
+                
+                let sum = x! + y! + z!
+                
+                if (sum > violence) {
+                    self.steps++
+                   
+                }
+            })
+        }
+    }
+    
+    func stopCalculating() {
+        manager.stopAccelerometerUpdates()
+    }
+    
+}
